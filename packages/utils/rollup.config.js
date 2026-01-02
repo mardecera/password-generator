6dc5242d644986872e2d6be07166399822a75621
mardecera <18353569+mardecera@users.noreply.github.com>
2026-01-02 00:54:28 -0500

    chore: config packages

---
+++ packages/utils/rollup.config.js
@@ -0,0 +1,29 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+
+export default [
+	{
+		input: 'src/index.ts',
+		output: [
+			{ file: 'dist/index.js', format: 'esm', sourcemap: true },
+			{
+				file: 'dist/index.cjs',
+				format: 'cjs',
+				sourcemap: true,
+				exports: 'named',
+			},
+		],
+		plugins: [
+			resolve(),
+			commonjs(),
+			typescript({ tsconfig: './tsconfig.json', declaration: false }),
+		],
+	},
+	{
+		input: 'src/index.ts',
+		output: [{ file: 'dist/index.d.ts', format: 'es' }],
+		plugins: [dts()],
+	},
+]
