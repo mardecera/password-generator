544a081737e7451249a19a3966e0cae0d876793a
Jonathan Cervantes A. <18353569+mardecera@users.noreply.github.com>
2026-02-16 10:47:14 -0500

    feat: improve utils (#20)

--- apps/web/src/i18n/routing.ts
+++ apps/web/src/i18n/routing.ts
@@ -1,6 +1,10 @@
 import { defineRouting } from 'next-intl/routing'
 
+export const locales = ['es', 'en'] as const
+
+// export type Locale = typeof locales[number]
+
 export const routing = defineRouting({
-	locales: ['es', 'en'],
+	locales,
 	defaultLocale: 'es',
 })
--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -1,25 +1,18 @@
 import { routing } from './i18n/routing'
 import { NextRequest, NextResponse } from 'next/server'
+import { hasLocalePrefix } from './utils/type-guards.utils'
 
 const PUBLIC_FILE = /\.[^/]+$/
+const PUBLIC_DIR = ['/_next', '/api', '/static']
 
 export default async function proxy(request: NextRequest) {
 	const { pathname } = request.nextUrl
+	const isPublicDir = PUBLIC_DIR.some((dir) => pathname.startsWith(dir))
+	const isPublicFile = PUBLIC_FILE.test(pathname)
 
-	if (
-		pathname.startsWith('/_next') ||
-		pathname.startsWith('/api') ||
-		pathname.startsWith('/static') ||
-		PUBLIC_FILE.test(pathname)
-	) {
-		return
-	}
+	if (isPublicDir || isPublicFile) return
 
-	const hasLocalePrefix = routing.locales.some(
-		(locale) => pathname === `/${locale}` || pathname.startsWith(`/${locale}/`)
-	)
-
-	if (!hasLocalePrefix) {
+	if (!hasLocalePrefix(pathname)) {
 		const url = request.nextUrl.clone()
 		url.pathname = `/${routing.defaultLocale}${pathname === '/' ? '' : pathname}`
 
